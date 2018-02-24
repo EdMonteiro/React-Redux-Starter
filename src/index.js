f437ed9e3f7d3e4d539be4fac7505f6214e3facd
Edemir Monteiro <edemirmonteiro@gmail.com>
2018-02-24 12:50:34 +0000

    Change foulder structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAQCLS9AYvMa65yiDZUf3wMNr4Zym9MDpw'; //Youtube API key
+const API_KEY = 'AIzaSyDpOMLBwsyejxP_QHUp0LotZfzhwcHOknQ'; //Youtube API key
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch (
+      {key: API_KEY, term: term}, (videos) => {
+        this.setState ({
+          videos,
+          selectedVideo: videos[0]
+        });
+      }
+    );
+  }
+
+  render () {
+    const videoSearch = _.debounce( (term) => { this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList  
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos} />
+      </div>
+    )
+  }
 }
+
 
 ReactDOM.render(
   <App />,
